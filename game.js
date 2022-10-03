d1db6f0a23aed0bf2fb9ae9ba39a570c87ecfac2
Aekangi Patel <aekangi91@gmail.com>
2022-10-03 16:12:06 -0400

    randomly picks flavor and dash sign done so far

---
+++ game.js
@@ -0,0 +1,42 @@
+const alphabet = document.querySelector('.alphabet')
+const dashes = document.querySelector('.dashes')
+let word = ''
+const maxLives = 6
+let livesLeft = 6
+let letterGuessed = []
+
+let flavors = [
+  'mint',
+  'vanilla',
+  'chocolate',
+  'strawberry',
+  'coffee',
+  'pistachio'
+]
+
+// function to pick a flavor at random
+const pickFlavor = () => {
+  word = flavors[Math.floor(Math.random() * flavors.length)]
+}
+
+// get the dashes for the word to show- (different #of dashes per word)
+const dashSign = () => {
+  const flavorIndex = word
+    .split('')
+    .map((alphabet) => {
+      if (letterGuessed.indexOf(alphabet) < 0) {
+        return '-'
+      } else {
+        return alphabet
+      }
+    })
+    .join('')
+  dashes.innerHTML = flavorIndex
+}
+
+// letter be replaced
+
+// functions to be called- i put them in the order that i'm writing the function..but put them accordingly!!!!
+
+pickFlavor()
+dashSign()
