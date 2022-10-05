4f050b678e6abcb7a0cb340493dea4a6d998df57
Aekangi Patel <aekangi91@gmail.com>
2022-10-05 11:44:50 -0400

    got the fake images in and function to work. replace these once real images are ready. need to work on img size

--- game.js
+++ game.js
@@ -25,12 +25,10 @@ const x = document.getElementById('X')
 const y = document.getElementById('Y')
 const z = document.getElementById('Z')
 
-const begin = document.querySelector('begin')
-
 const startOver = document.getElementById('play')
 const gameOver = document.getElementById('gameOverText')
 const livesLeft = document.querySelector('.livesLeft')
-livesLeft.innerText = 6
+livesLeft.innerHTML = 6
 const alph = document.querySelectorAll('.alph')
 let alphArray = Array.from(alph)
 const dashes = document.querySelector('.dashes')
@@ -52,19 +50,35 @@ const pickFlavor = () => {
 }
 pickFlavor()
 
+const gameWon = () => {
+  if (letterGuessed === word) {
+    gameOver.innerHTML = 'Yay the sundae survived!!'
+  }
+}
+
+const lostGame = () => {
+  if (parseInt(livesLeft.innerHTML) <= 0) {
+    gameOver.innerHTML = `The sundae disappeared, you lost! The answer was ${word}`
+  }
+}
 const getValue = () => {
-  let letter = event.target.innerText
+  let letter = event.target.innerHTML
   letterGuessed.push(letter)
   let guess = letterGuessed[letterGuessed.length - 1].toLowerCase()
   if (word.includes(letter)) {
     dashSign()
     gameWon()
+    // disable()
   } else {
     livesLeft.innerHTML = livesLeft.innerHTML - 1
+    // disable()
+    sundaeImage()
     lostGame()
   }
 }
-//
+// function disable() {
+//   a.disabled = true
+// }
 
 const dashSign = () => {
   const flavorIndex = word
@@ -81,17 +95,38 @@ const dashSign = () => {
 }
 dashSign()
 
-const gameWon = () => {
-  if (letterGuessed === word) {
-    gameOver.innerHTML = 'Yay the sundae survived!!'
+const sundaeImage = () => {
+  console.log(livesLeft.innerHTML)
+  if (livesLeft.innerHTML === '6') {
+    document.getElementById('cherry').src =
+      'https://images.unsplash.com/photo-1528821154947-1aa3d1b74941?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlcnJ5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+  } else if (livesLeft.innerHTML === '5') {
+    document.getElementById('cherry').src = ''
+    document.getElementById('sprinkles').src =
+      'https://images.unsplash.com/photo-1646318753516-078a7ddcc770?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aWNlJTIwY3JlYW0lMjBjb25lJTIwc3VuZGFlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+  } else if (livesLeft.innerHTML === '4') {
+    document.getElementById('sprinkles').src = ''
+    document.getElementById('hotFudge').src =
+      'https://plus.unsplash.com/premium_photo-1661286645287-b7084e5a41d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aWNlJTIwY3JlYW0lMjBjb25lJTIwc3VuZGFlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+  } else if (livesLeft.innerHTML === '3') {
+    document.getElementById('hotFudge').src = ''
+    document.getElementById('twoScoops').src =
+      'https://images.unsplash.com/photo-1477505982272-ead89926a577?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aWNlJTIwY3JlYW0lMjBjb25lJTIwd2l0aCUyMHNjb29wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+  } else if (livesLeft.innerHTML === '2') {
+    document.getElementById('twoScoops').src = ''
+    document.getElementById('oneScoop').src =
+      'https://images.unsplash.com/photo-1526549518783-1763d32c7763?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGljZSUyMGNyZWFtJTIwY29uZSUyMHdpdGglMjBzY29vcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+  } else if (livesLeft.innerHTML === '1') {
+    document.getElementById('oneScoop').src = ''
+    document.getElementById('cone').src =
+      'https://images.unsplash.com/photo-1559703248-dcaaec9fab78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
+  } else {
+    document.getElementById('cone').src = ''
+    document.getElementById('poof').src =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo9aA_f-szG7GR7vqhAjojQtXMCH6i4XMZ6Q&usqp=CAU'
   }
 }
-
-const lostGame = () => {
-  if (parseInt(livesLeft.innerHTML) <= 0) {
-    gameOver.innerHTML = `The sundae disappeared, you lost! The answer was ${word}`
-  }
-}
+sundaeImage()
 
 const restart = () => {
   location.reload()
